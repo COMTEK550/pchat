7fd7bcfb89d4a545d8809cf9c0e3e44eba3ad4a5
Julian T <julian@jtle.dk>
2020-10-20 10:54:56 +0200

    Added simple test client

--- src/client/Main.java
+++ src/client/Main.java
@@ -14,22 +14,17 @@ public class Main {
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
+
         try (
                 Socket echoSocket = new Socket(hostName, portNumber);
-                PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
+
+                ObjectOutputStream out = new ObjectOutputStream(echoSocket.getOutputStream());
+
         ) {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo: " + in.readLine());
-            }
+            out.writeObject(new TextMessage("Hej med dig fra client"));
+            out.writeObject(new TextMessage("Besked 2"));
+            out.close();
+            echoSocket.close();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
