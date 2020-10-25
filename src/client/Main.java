831a38e99532352997943281a9a4b14a1ae30ea1
Victor Büttner <victor@0x23.dk>
2020-10-25 14:30:22 +0100

    add initial GUI stuff

--- src/client/Main.java
+++ src/client/Main.java
@@ -2,5 +2,10 @@
 class Main {
     public static void main(String[] args) {
         Hello.Do("Client");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new rootGUI().setVisible(true);
+            }
+        });
     }
 }
