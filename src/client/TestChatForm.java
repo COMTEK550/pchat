a010e24bbacb879be1534a42bdba095d560baff5
Victor Büttner <victor@0x23.dk>
2020-10-27 14:10:32 +0100

    failing to setup test

---
+++ src/client/TestChatForm.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.Assert.assertEquals;
+
+public class TestChatForm extends org.junit.TestCase {
+  protected int value1, value2;
+
+  @Test
+  public void testTests() {
+    String lol = "hej";
+    System.out.println(lol);
+    assertEquals(lol, lol);
+  }
+}
